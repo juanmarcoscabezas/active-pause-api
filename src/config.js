fde03554ac1056685ac865c9f28aae1383a038a0
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-04-14 13:23:25 -0500

    Removing config

--- src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "dev": {
-        "mongoUrl": "mongodb://localhost:27017/activepause",
-        "secretKey": "my-secret-key",
-        "host": "localhost",
-        "port": 3010
-    }
-}
